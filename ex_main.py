d47fe8ef3e77fd7998c48d918edecb6cf6dcddd3
Jeonsangeun <44052428+Jeonsangeun@users.noreply.github.com>
2019-03-16 20:01:56 +0900

    Add files via upload

---
+++ ex_main.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+import numpy as np
+import random as rd
+import ex_env as cache
+import ex_DQN as DQN
+from collections import deque
+import matplotlib.pyplot as plt
+
+env = cache.cache_replacement()
+
+input_size = 4
+output_size = 8
+dis = .85
+request = 1000
+REPLAY_MEMORY = 10000000
+x_layer = []
+y_layer = []
+
+def replay_train(mainDQN, targetDQN, train_batch):
+    x_stack = np.empty(0).reshape(0, input_size)
+    y_stack = np.empty(0).reshape(0, output_size)
+
+    for state, action, reward, next_state, done in train_batch:
+        Q = mainDQN.predict(state)
+
+        if done:
+            Q[0, action] = reward
+        else:
+            Q[0, action] = reward + dis * np.max(targetDQN.predict(next_state))
+
+        y_stack = np.vstack([y_stack, Q])
+        x_stack = np.vstack([x_stack, state])
+
+    return mainDQN.update(x_stack, y_stack)
+
+def get_copy_var_ops(*, dest_scope_name = "target", src_scope_name="main"):
+    op_holder = []
+
+    src_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=src_scope_name)
+    dest_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=dest_scope_name)
+
+    for src_var, dest_var in zip(src_vars, dest_vars):
+        op_holder.append(dest_var.assign(src_var.value()))
+
+    return op_holder
+
+def main():
+    max_episodes = 5000
+    replay_buffer = deque()
+    cost = 0
+
+    with tf.Session() as sess:
+        mainDQN = DQN.DQN(sess, input_size, output_size, name="main")
+        targetDQN = DQN.DQN(sess, input_size, output_size, name="target")
+        tf.global_variables_initializer().run()
+
+        copy_ops = get_copy_var_ops(dest_scope_name="target", src_scope_name="main")
+        sess.run(copy_ops)
+        for episode in range(max_episodes):
+            e = 1. / ((episode / 200) + 1)
+            state, file, user = env.reset()
+            for i in range(request):
+                dev = user
+                '''
+                if np.random.rand(1) < e:
+                    action = env.random_action()
+                else:
+                    action = np.argmax(mainDQN.predict(state))
+                '''
+                print(env.state)
+                print(state)
+                print(mainDQN.predict(state))
+                action = int(input("액션을 입력하시오"))
+                print("---------------------")
+                next_state, reward, done, file, user = env.step(action, file, user)
+                print(env.state)
+                print(env.cost)
+                print(next_state)
+                print(replay_buffer)
+                if dev == 10:
+                    if 0. in env.state[0]:
+                        pass
+                    else:
+                        replay_buffer.append((state, action, reward, next_state, done))
+                else:
+                    if 0. in env.state[1]:
+                        pass
+                    else:
+                        replay_buffer.append((state, action, reward, next_state, done))
+                if len(replay_buffer) > REPLAY_MEMORY:
+                    replay_buffer.popleft()
+            cost += env.cost
+            if episode % 100 == 99:
+                x_layer.append(episode / 100)
+                y_layer.append(cost / 100)
+                print("Episode: {} cost: {}".format(episode, (cost / 100)))
+                for _ in range(50):
+                    minibatch = rd.sample(replay_buffer, 50)
+                    loss, _ = replay_train(mainDQN, targetDQN, minibatch)
+                print("Loss: ", loss)
+                cost = 0
+                sess.run(copy_ops)
+
+        result = mainDQN.Weight.eval()
+        Bias = mainDQN.Bias.eval()
+        np.save('Cache_Weight', result)
+        np.save('Bias', Bias)
+
+    plt.plot(x_layer, y_layer)
+    plt.show()
+
+if __name__ =="__main__":
+    main()
