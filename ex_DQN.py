73eb3f11ce4169ccbcf9993299c67b2c98166a17
Jeonsangeun <44052428+Jeonsangeun@users.noreply.github.com>
2019-03-19 20:35:14 +0900

    Update ex_DQN.py

--- ex_DQN.py
+++ ex_DQN.py
@@ -10,12 +10,11 @@ class DQN:
         self.input_size = input_size
         self.output_size = output_size
         self.net_name = name
-        self.Weight_1 = []
-        self.Bias = []
+        self.Weight = []
 
         self._build_network()
 
-    def _build_network(self, h_size=20, l_rate=0.01):
+    def _build_network(self, h_size=50, l_rate=0.01):
         with tf.variable_scope(self.net_name):
             self._X = tf.placeholder(tf.float32, [None, self.input_size], name="input_x")
 
@@ -23,18 +22,23 @@ class DQN:
                                  initializer=tf.contrib.layers.xavier_initializer())
             b1 = tf.Variable(tf.random_normal([h_size]))
             layer1 = tf.nn.tanh(tf.matmul(self._X, W1) + b1)
-            W2 = tf.get_variable("W3", shape=[h_size, self.output_size],
+            W2 = tf.get_variable("W2", shape=[h_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
-            b2 = tf.Variable(tf.random_normal([self.output_size]))
+            b2 = tf.Variable(tf.random_normal([h_size]))
+            layer2 = tf.nn.tanh(tf.matmul(layer1, W2) + b2)
+            W3 = tf.get_variable("W3", shape=[h_size, self.output_size],
+                                 initializer=tf.contrib.layers.xavier_initializer())
+            b3 = tf.Variable(tf.random_normal([self.output_size]))
 
-            self._Qpred = tf.matmul(layer1, W2) + b2
+            self._Qpred = tf.matmul(layer2, W3) + b3
 
         self._Y = tf.placeholder(shape=[None, self.output_size], dtype=tf.float32)
         self._loss = tf.reduce_mean(tf.square(self._Y - self._Qpred))
         self._train = tf.train.AdamOptimizer(learning_rate=l_rate).minimize(self._loss)
 
         self.Weight_1 = tf.matmul(W1, W2)
-        self.Bias = b2
+        self.Weight = tf.matmul(self.Weight_1, W3)
+        self.Bias = b3
 
     def predict(self, state):
         x = np.reshape(state, [1, self.input_size])
