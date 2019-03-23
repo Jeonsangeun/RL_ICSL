a945bb3f18232ba72027b635ae589443b082f6ad
Jeonsangeun <44052428+Jeonsangeun@users.noreply.github.com>
2019-03-23 19:41:30 +0900

    Update Cache_main.py

--- Cache_main.py
+++ Cache_main.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import random as rd
-import ex_env as cache
-import ex_DQN as DQN
+import Cache_env as cache
+import Cache_DQN as DQN
 from collections import deque
 import matplotlib.pyplot as plt
 
@@ -11,7 +11,7 @@ env = cache.cache_replacement()
 input_size = 10
 output_size = 4
 dis = .90
-request = 1000
+request = 2000
 REPLAY_MEMORY = 5000000
 x_layer = []
 y_layer = []
@@ -45,7 +45,7 @@ def get_copy_var_ops(*, dest_scope_name = "target", src_scope_name="main"):
     return op_holder
 
 def main():
-    max_episodes = 1000
+    max_episodes = 25000
     replay_buffer = deque()
     cost = 0
 
@@ -61,6 +61,7 @@ def main():
             state, file, user = env.reset()
             for i in range(request):
                 #action = int(input("액션을 입력하시오"))
+
                 if np.random.rand(1) < e:
                     action = env.random_action()
                 else:
@@ -70,30 +71,37 @@ def main():
                     tmp_state = env.state[0].copy()
                     aa = np.where(env.state[0] == file)
                     bb = np.max(env.state[0])
-                    if bb == file:
-                        tmp_state[aa[0][0]] = -1000
-                        bb = np.max(tmp_state)
-                        cc = np.where(tmp_state == bb)
-                        action = cc[0][0]
+                    if 0. in tmp_state:
+                        action = 0
                     else:
-                        cc = np.where(tmp_state == bb)
-                        action = cc[0][0]
+                        if bb == file:
+                            tmp_state[aa[0][0]] = -1000
+                            bb = np.max(tmp_state)
+                            cc = np.where(tmp_state == bb)
+                            action = cc[0][0]
+                        else:
+                            cc = np.where(tmp_state == bb)
+                            action = cc[0][0]
                 if user == -10:
                     tmp_state = env.state[1].copy()
                     aa = np.where(env.state[1] == file)
                     bb = np.max(env.state[1])
-                    if bb == file:
-                        tmp_state[aa[0][0]] = -1000
-                        bb = np.max(tmp_state)
-                        cc = np.where(tmp_state == bb)
-                        action = cc[0][0]
+                    if 0. in tmp_state:
+                        action = 0
                     else:
-                        cc = np.where(tmp_state == bb)
-                        action = cc[0][0]
+                        if bb == file:
+                            tmp_state[aa[0][0]] = -1000
+                            bb = np.max(tmp_state)
+                            cc = np.where(tmp_state == bb)
+                            action = cc[0][0]
+                        else:
+                            cc = np.where(tmp_state == bb)
+                            action = cc[0][0]
                 '''
                 #print(env.state)
                 #print(action)
-                #print(env.state)
+                #print(file)
+                #print(user)
                 #print("---------------------")
                 next_state, reward, done, file, user = env.step(action, file, user)
                 #print(reward)
@@ -113,23 +121,31 @@ def main():
                 y_layer.append(cost / 100)
                 print("Episode: {} cost: {}".format(episode, (cost / 100)))
                 for _ in range(50):
-                    minibatch = rd.sample(replay_buffer, 100)
+                    minibatch = rd.sample(replay_buffer, 200)
                     loss, _ = replay_train(mainDQN, targetDQN, minibatch)
                 print("Loss: ", loss)
                 cost = 0
                 sess.run(copy_ops)
 
-        result = mainDQN.Weight.eval()
-        Bias = mainDQN.Bias.eval()
-        np.save('Cache_Weight_1', result)
-        np.save('Bias_1', Bias)
+        W1 = mainDQN.Weight_1.eval()
+        W2 = mainDQN.Weight_2.eval()
+        W3 = mainDQN.Weight_3.eval()
+        B1 = mainDQN.Bias_1.eval()
+        B2 = mainDQN.Bias_2.eval()
+        B3 = mainDQN.Bias_3.eval()
+        np.save('Cache_W_1', W1)
+        np.save('Cache_W_2', W2)
+        np.save('Cache_W_3', W3)
+        np.save('B_1', B1)
+        np.save('B_2', B2)
+        np.save('B_3', B3)
+
+    np.save("X_", x_layer)
+    np.save("Y_", y_layer)
 
     plt.plot(x_layer, y_layer)
     plt.show()
 
 if __name__ =="__main__":
     main()
-
-
-
 
