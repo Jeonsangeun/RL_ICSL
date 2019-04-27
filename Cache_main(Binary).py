e235504d9b59098068cc7d0d409f9623460578c0
Jeonsangeun <44052428+Jeonsangeun@users.noreply.github.com>
2019-04-27 16:39:57 +0900

    Update Cache_main(Binary).py

--- Cache_main(Binary).py
+++ Cache_main(Binary).py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 
 env = cache.cache_replacement()
 
-input_size = 22
-output_size = 10
-dis = .96
-request = 200
-REPLAY_MEMORY = 1000000
+input_size = env.F_packet + 2
+output_size = env.F_packet + 1
+dis = .9999
+request = (100 * env.Num_packet)
+REPLAY_MEMORY = 500000
 x_layer = []
 y_layer = []
 
@@ -48,6 +48,7 @@ def main():
     max_episodes = 30000
     replay_buffer = deque()
     cost = 0
+    env.Zip_funtion()
 
     with tf.Session() as sess:
         mainDQN = DQN.DQN(sess, input_size, output_size, name="main")
@@ -57,16 +58,20 @@ def main():
         copy_ops = get_copy_var_ops(dest_scope_name="target", src_scope_name="main")
         sess.run(copy_ops)
         for episode in range(max_episodes):
-            e = 1. / ((episode / 500) + 1)
+            e = 1. / ((episode // 1000) + 1)
             state, file, user = env.reset()
+            #print(np.argmax(mainDQN.predict(state)))
             for i in range(request):
-                #action = int(input("액션을 입력하시
 
                 if np.random.rand(1) < e:
                     action = env.random_action()
                 else:
-                    action = np.argmax(mainDQN.predict(state))
+                    action = np.argmax(mainDQN.predict(state)[0])
+                    #if episode > 1000:
+                    #    print(action)
+                    #    print(mainDQN.predict(state)[0])
                 '''
+                action = 0
                 if user == 1:
                     tmp = env.state[0].copy()
                     if -1 in tmp:
@@ -89,13 +94,16 @@ def main():
                             action = np.max(tmp)
                         else:
                             action = aa
-                '''#비인기 패킷 제거
+                '''
+                #비인기 패킷 제거
                 #print(env.state)
-                #print("action", action)
                 #print("packet", file)
                 #print(state)
                 #print("user: ", user)
                 #print("---------------------")
+                #action = int(input("액션을 입력하시오: "))
+                #print("action", action)
+                #print((state == 1).sum())
                 next_state, reward, done, file, user = env.step(action, file, user)
                 #print(reward)
                 #print(env.state)
@@ -129,18 +137,41 @@ def main():
                 cost = 0
                 sess.run(copy_ops)
 
+        state, file, user = env.reset()
+        for i in range(request):
+            print(env.state)
+            action = np.argmax(mainDQN.predict(state))
+            print("action", action)
+            print(state)
+            print("user: ", user)
+            print("packet", file)
+            print("---------------------")
+            next_state, reward, done, file, user = env.step(action, file, user)
+            print(reward)
+            print(env.state)
+            print(env.cost)
+            state = next_state
+
         W1 = mainDQN.Weight_1.eval()
         W2 = mainDQN.Weight_2.eval()
         W3 = mainDQN.Weight_3.eval()
+        W4 = mainDQN.Weight_4.eval()
+        W5 = mainDQN.Weight_5.eval()
         B1 = mainDQN.Bias_1.eval()
         B2 = mainDQN.Bias_2.eval()
         B3 = mainDQN.Bias_3.eval()
-        np.save('Cache_W_1', W1)
-        np.save('Cache_W_2', W2)
-        np.save('Cache_W_3', W3)
-        np.save('B_1', B1)
-        np.save('B_2', B2)
-        np.save('B_3', B3)
+        B4 = mainDQN.Bias_4.eval()
+        B5 = mainDQN.Bias_5.eval()
+        np.save('Cache_W_1@', W1)
+        np.save('Cache_W_2@', W2)
+        np.save('Cache_W_3@', W3)
+        np.save('Cache_W_4@', W4)
+        np.save('Cache_W_5@', W5)
+        np.save('B_1!', B1)
+        np.save('B_2!', B2)
+        np.save('B_3!', B3)
+        np.save('B_4!', B4)
+        np.save('B_5!', B5)
 
     np.save("X_", x_layer)
     np.save("Y_", y_layer)
