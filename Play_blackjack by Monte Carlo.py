320feab5ecc583e9b87ecfb8d133f4d7e456a789
Jeonsangeun <44052428+Jeonsangeun@users.noreply.github.com>
2019-05-17 14:18:36 +0900

    Update Play_blackjack by Monte Carlo.py

--- Play_blackjack by Monte Carlo.py
+++ Play_blackjack by Monte Carlo.py
@@ -2,13 +2,16 @@ import Black_jack as Black
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import time
+start_time = time.time()
 
 env = Black.Black()
 
 print("Black-Jack Play")
 Value = np.zeros([10, 10, 2])
 Count = np.zeros([10, 10, 2])
-max_episode = 1000000
+max_episode = 100000
+dis = 0
 
 for episode in range(max_episode):
     state = env.reset()
@@ -40,12 +43,20 @@ for episode in range(max_episode):
         print("딜러 점수:", env.Count_Point(env.Dealer))
         print("플레이어의 패", env.Player)
         print("플레이어 점수:", env.Count_Point(env.Player))
+        
+        
+        Count[state] += 1
+        Value[state] = Value[state] + (1 / Count[state]) * (reward + dis * Value[new_state] - Value[state])
         '''
         Return += reward
         if done == True:
             Count[start] += 1
             Value[start] = Value[start] + (1 / Count[start]) * (Return - Value[start])
+
         state = new_state
+
+
+
         #print("reward:", reward)
 
     if episode % 1000 == 0:
@@ -55,16 +66,17 @@ x = np.array([9, 0, 1, 2, 3, 4, 5, 6, 7, 8])
 y = np.array(range(10))
 xx, yy = np.meshgrid(x, y)
 
-z_1 = Value[xx, yy, 0]
-z_2 = Value[xx, yy, 1]
+z_1 = Value[xx, yy, env.Usable_Ace]
 
 x_1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 y_1 = np.array(range(12, 22, 1))
 xx_1, yy_1 = np.meshgrid(x_1, y_1)
 
+print("--- %s seconds ---" % (time.time() - start_time))
+
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.set_title("Black_jack value-function")
 ax.plot_wireframe(xx_1, yy_1, z_1, rstride=1, cstride=1)
-#ax.plot_wireframe(xx_1, yy_1, z_2, rstride=1, cstride=1)
 plt.show()
+
