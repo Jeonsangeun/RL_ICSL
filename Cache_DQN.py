5dcd860eff1f749ddafee61f30ef123a690aca9b
Jeonsangeun <44052428+Jeonsangeun@users.noreply.github.com>
2019-06-03 13:33:42 +0900

    Update Cache_DQN.py

--- Cache_DQN.py
+++ Cache_DQN.py
@@ -22,52 +22,52 @@ class DQN:
             W1 = tf.get_variable("W1", shape=[self.input_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
             b1 = tf.Variable(tf.random_normal([h_size]))
-            layer1 = tf.nn.relu(tf.matmul(self._X, W1)+b1)
+            layer1 = tf.nn.leaky_relu(tf.matmul(self._X, W1)+b1)
             layer1 = tf.nn.dropout(layer1, rate=self.dropout_rate)
             W2 = tf.get_variable("W2", shape=[h_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
             b2 = tf.Variable(tf.random_normal([h_size]))
-            layer2 = tf.nn.relu(tf.matmul(layer1, W2)+b2)
+            layer2 = tf.nn.leaky_relu(tf.matmul(layer1, W2)+b2)
             layer2 = tf.nn.dropout(layer2, rate=self.dropout_rate)
             W3 = tf.get_variable("W3", shape=[h_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
             b3 = tf.Variable(tf.random_normal([h_size]))
-            layer3 = tf.nn.relu(tf.matmul(layer2, W3)+b3)
+            layer3 = tf.nn.leaky_relu(tf.matmul(layer2, W3)+b3)
             layer3 = tf.nn.dropout(layer3, rate=self.dropout_rate)
             W4 = tf.get_variable("W4", shape=[h_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
             b4 = tf.Variable(tf.random_normal([h_size]))
-            layer4 = tf.nn.relu(tf.matmul(layer3, W4)+b4)
+            layer4 = tf.nn.leaky_relu(tf.matmul(layer3, W4)+b4)
             layer4 = tf.nn.dropout(layer4, rate=self.dropout_rate)
             W5 = tf.get_variable("W5", shape=[h_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
             b5 = tf.Variable(tf.random_normal([h_size]))
-            layer5 = tf.nn.relu(tf.matmul(layer4, W5)+b5)
+            layer5 = tf.nn.leaky_relu(tf.matmul(layer4, W5)+b5)
             layer5 = tf.nn.dropout(layer5, rate=self.dropout_rate)
             W6 = tf.get_variable("W6", shape=[h_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
             b6 = tf.Variable(tf.random_normal([h_size]))
-            layer6 = tf.nn.relu(tf.matmul(layer5, W6) + b6)
+            layer6 = tf.nn.leaky_relu(tf.matmul(layer5, W6) + b6)
             layer6 = tf.nn.dropout(layer6, rate=self.dropout_rate)
             W7 = tf.get_variable("W7", shape=[h_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
             b7 = tf.Variable(tf.random_normal([h_size]))
-            layer7 = tf.nn.relu(tf.matmul(layer6, W7) + b7)
+            layer7 = tf.nn.leaky_relu(tf.matmul(layer6, W7) + b7)
             layer7 = tf.nn.dropout(layer7, rate=self.dropout_rate)
             W8 = tf.get_variable("W8", shape=[h_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
             b8 = tf.Variable(tf.random_normal([h_size]))
-            layer8 = tf.nn.relu(tf.matmul(layer7, W8) + b8)
+            layer8 = tf.nn.leaky_relu(tf.matmul(layer7, W8) + b8)
             layer8 = tf.nn.dropout(layer8, rate=self.dropout_rate)
             W9 = tf.get_variable("W9", shape=[h_size, h_size],
                                  initializer=tf.contrib.layers.xavier_initializer())
             b9 = tf.Variable(tf.random_normal([h_size]))
-            layer9 = tf.nn.relu(tf.matmul(layer8, W9) + b9)
+            layer9 = tf.nn.leaky_relu(tf.matmul(layer8, W9) + b9)
             layer9 = tf.nn.dropout(layer9, rate=self.dropout_rate)
             W10 = tf.get_variable("W10", shape=[h_size, h_size],
                                   initializer=tf.contrib.layers.xavier_initializer())
             b10 = tf.Variable(tf.random_normal([h_size]))
-            layer10 = tf.nn.relu(tf.matmul(layer9, W10) + b10)
+            layer10 = tf.nn.leaky_relu(tf.matmul(layer9, W10) + b10)
             layer10 = tf.nn.dropout(layer10, rate=self.dropout_rate)
             W11 = tf.get_variable("W11", shape=[h_size, self.output_size],
                                   initializer=tf.contrib.layers.xavier_initializer())
